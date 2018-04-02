494647c8be2f551ac49a397d8bd951f4e05a1c78
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-02 16:52:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = "I don't do much."
 
 var kittens = ['Milo', 'Otis', 'Garfield']
 
-function destructivelyAppendKitten(kittens, 'Rafe') {
+function destructivelyAppendKitten(kittens, Rafe) {
   kittens.push('Rafe')
   return 
 }
